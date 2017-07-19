53ff2cb5de70c25213dc16ffa42b4bea0fd05402
Eliaquimoriundomallcco <30282944+Eliaquimoriundomallcco@users.noreply.github.com>
2017-07-19 01:44:26 -0500

    version 1

---
+++ mitienda/Areas/Admin/Controllers/ProductoController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using mitienda.Models;
+using System.IO;
+
+namespace mitienda.Areas.Admin.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private mitiendaEntities db = new mitiendaEntities();
+
+        // GET: Admin/Producto
+        public ActionResult Index()
+        {
+            return View(db.Producto.ToList());
+        }
+
+        // GET: Admin/Producto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: Admin/Producto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Producto/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductoId,Denominacion,Descripcion,Precio,Existencias,Activo")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Producto.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(producto);
+        }
+
+        // GET: Admin/Producto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Admin/Producto/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductoId,Denominacion,Descripcion,Precio,Existencias,Activo")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(producto);
+        }
+
+        // GET: Admin/Producto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Admin/Producto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Producto producto = db.Producto.Find(id);
+            db.Producto.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+        public JsonResult Adjuntar(int ProductoId, HttpPostedFileBase documento)
+        {
+            var respuesta = new Models.ResponseModel
+            {
+                respuesta = true,
+                error = ""
+            };
+
+            if (documento != null)
+            {
+                string adjunto = DateTime.Now.ToString("yyyyMMddHHmmss") + Path.GetExtension(documento.FileName);
+                documento.SaveAs(Server.MapPath("~/ImgProductos/" + adjunto));
+
+                db.ProductoImagen.Add(new ProductoImagen { ProductoId = ProductoId, Imagen = adjunto, Titulo = "Ejemplo", Descripcion = "Ejemplo" });
+                db.SaveChanges();
+
+            }
+            else
+            {
+                respuesta.respuesta = false;
+                respuesta.error = "Debe adjuntar un documento";
+            }
+
+            return Json(respuesta);
+        }
+
+        public PartialViewResult Adjuntos(int ProductoId)
+        {
+            return PartialView(db.ProductoImagen.Where(x => x.ProductoId == ProductoId).ToList());
+        }
+
+
+        public JsonResult EliminarImagen(int ProductoImagenId)
+        {
+            var rpt = new Models.ResponseModel()
+            {
+                respuesta = true,
+                error = ""
+            };
+            var img = db.ProductoImagen.Find(ProductoImagenId);
+
+            if (System.IO.File.Exists(Server.MapPath("~/ImgProductos/" + img.Imagen)))
+                System.IO.File.Delete(Server.MapPath("~/ImgProductos/" + img.Imagen));
+
+            db.ProductoImagen.Remove(img);
+            db.SaveChanges();
+
+            return Json(rpt);
+        }
+
+
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ mitienda/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace mitienda.Controllers
+{
+    public class HomeController : Controller
+
+
+    {
+
+        private Models.mitiendaEntities bd = new Models.mitiendaEntities();
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Buscar(String id = "")
+        {
+            //logica de acceso a bd
+
+            var productos = bd.Producto.Where(X => X.Descripcion.Contains(id)).Take(12).ToList();
+
+            //lista de productos
+            ViewBag.buscar = id;
+            return View(productos);
+          
+
+        }
+
+       // public ActionResult Producto( String id)
+        //{
+            
+
+       //    ViewBag.producto = id;
+         //   return View();
+        //}
+    }
+}
